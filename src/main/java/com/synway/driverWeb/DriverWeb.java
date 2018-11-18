9e432934e2c853ec8438a40880119042a369dbb2
Moveyulu <1252345930@qq.com>
2018-11-18 20:23:24 +0800

    commit Directory

--- src/main/java/com/synway/driverWeb/DriverWeb.java
+++ src/main/java/com/synway/driverWeb/DriverWeb.java
@@ -21,6 +21,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
 
 public class DriverWeb {
 
